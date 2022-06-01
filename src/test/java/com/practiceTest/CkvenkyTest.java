f46433c1df3c1604de98cc096e387239f8c48d21
Chandankumar <chandanraayappab@gmail.com>
2022-06-01 17:11:59 +0530

    first commit

---
+++ src/test/java/com/practiceTest/CkvenkyTest.java
@@ -0,0 +1,12 @@
+package com.practiceTest;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CkvenkyTest {
+	@Test
+	public void ckvenky() {
+		Reporter.log("hai venky hello ck",true);
+	}
+
+}
